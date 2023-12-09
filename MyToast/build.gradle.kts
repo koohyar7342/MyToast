1bf55bef32ee08b6ce290766517f7756fc1ada58
koohyar <moslem.tazia@gmail.com>
2023-12-09 04:09:11 +0330

    publish version

--- MyToast/build.gradle.kts
+++ MyToast/build.gradle.kts
@@ -34,7 +34,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.koohyar"
             artifactId = "MyToast"
-            version = "1.0.0"
+            version = "1.0.1"
 
             afterEvaluate {
                 from(components["release"])
